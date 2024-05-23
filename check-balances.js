da5b59092c49e52e36a24bb13ff0189125942295
Null <93053007+Cnull00@users.noreply.github.com>
2024-05-24 05:49:31 +0700

    Update check-balances.js

--- check-balances.js
+++ check-balances.js
@@ -1,42 +1,28 @@
-const fs = require('fs');
-const { Alchemy, Network } = require('alchemy-sdk');
-require('colors');
+const fs = require('fs')
+const ethers = require('ethers')
+require('colors')
 
-const settings = {
-    apiKey: "Ii_Z2-PK1HdkR3-qk4yN8LIyd6u_pn8X",
-    network: Network.ETH_MAINNET,
-};
-
-const alchemy = new Alchemy(settings);
+const provider = new ethers.providers.WebSocketProvider(
+    'wss://eth-mainnet.g.alchemy.com/v2/Ii_Z2-PK1HdkR3-qk4yN8LIyd6u_pn8X'
+)
 
 const addresses = fs
     .readFileSync('hits.txt', 'utf8')
     .split('\n')
     .map((val) => {
-        return val.split(',');
-    });
+        return val.split(',')
+    })
 
-(async () => {
+;(async () => {
     for (let i = 0; i < addresses.length; i++) {
-        const address = addresses[i][0];
-        try {
-            const balance = await alchemy.getTokenBalances(address); // Menggunakan Alchemy untuk mendapatkan saldo
-            if (balance.gt(0)) {
-                console.log(address.bgGreen.black, balance.toString().bgGreen.black);
-                console.log('Private Key: '.yellow, addresses[i][1]);
-            } else {
-                console.log(address, 0);
-            }
-        } catch (error) {
-            console.error(`Error retrieving balance for address ${address}: ${error.message}`.red);
+        const address = addresses[i][0]
+        const balance = await provider.getBalance(address)
+
+        if (balance.gt(0)) {
+            console.log(address.bgGreen.black, balance.toString().bgGreen.black)
+            console.log('Private Key: '.yellow, addresses[i][1])
+        } else {
+            console.log(address, 0)
         }
     }
-})();
-
-// Get the latest block
-const latestBlock = alchemy.getBlockNumber();
-
-// Get all outbound transfers for a provided address
-alchemy.getTokenBalances('${address}')
-    .then(console.log);
-);
+})()
